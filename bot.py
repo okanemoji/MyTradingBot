b8f5cf9b277ce54ad655ab92481ba83aad5e3e4f
okanemoji <somchai13.jp@gmail.com>
2026-03-15 19:21:19 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,6 @@ API_KEY = os.environ.get("BINANCE_API_KEY")
 API_SECRET = os.environ.get("BINANCE_API_SECRET")
 
 client = Client(API_KEY, API_SECRET)
-client.FUTURES_URL = "https://testnet.binancefuture.com/fapi"
 
 client.futures_change_leverage(
     symbol=SYMBOL,
